68bd32a1e861799bb07a7ca13ae0ef0d583861af
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-04-22 13:53:21 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
-  role_arn = module.iam.eks_fargate_task_execution_role_arn
+ role_arn = var.eks_fargate_task_execution_role_arn
  
   vpc_config {
     subnet_ids = var.subnet_ids
@@ -10,7 +10,7 @@ resource "aws_eks_cluster" "main" {
 resource "aws_eks_fargate_profile" "main" {
   cluster_name           = aws_eks_cluster.main.name
   fargate_profile_name   = var.fargate_profile_name
-  pod_execution_role_arn = module.iam.eks_fargate_task_execution_role_arn
+  pod_execution_role_arn = var.eks_fargate_task_execution_role_arn
   subnet_ids             = var.subnet_ids
  
   selector {
