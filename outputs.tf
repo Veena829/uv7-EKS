570ec3085a5e5a996e59599d8626974de9b7f05e
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-04-22 13:42:22 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,15 @@
-# modules/eks/outputs.tf
 output "cluster_name" {
-  value = aws_eks_cluster.main.name
+  value = module.eks.cluster_name
 }
 
 output "cluster_id" {
-  value = aws_eks_cluster.main.id
+  value = module.eks.cluster_id
 }
 
 output "cluster_endpoint" {
-  value = aws_eks_cluster.main.endpoint
+  value = module.eks.cluster_endpoint
 }
 
 output "cluster_security_group_id" {
-  value = aws_eks_cluster.main.cluster_security_group_id
+  value = module.eks.cluster_security_group_id
 }
