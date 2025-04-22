0260da8bb11b021221da5a4855bc49a8092c2579
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-04-22 14:57:39 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,31 +1,35 @@
+# Define the EKS Cluster
 resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
- role_arn = var.eks_fargate_task_execution_role_arn
- 
+  role_arn = var.eks_fargate_task_execution_role_arn
+
   vpc_config {
     subnet_ids = var.subnet_ids
   }
 }
- 
+
+# Define the Fargate Profile
 resource "aws_eks_fargate_profile" "main" {
   cluster_name           = aws_eks_cluster.main.name
   fargate_profile_name   = var.fargate_profile_name
   pod_execution_role_arn = var.eks_fargate_task_execution_role_arn
   subnet_ids             = var.subnet_ids
- 
+
   selector {
     namespace = "default"
   }
 }
+
+# Fetch EKS Cluster Data
 data "aws_eks_cluster" "main" {
   name = aws_eks_cluster.main.name
 }
- 
+
 data "aws_eks_cluster_auth" "main" {
   name = aws_eks_cluster.main.name
 }
- 
-# Configure Kubernetes provider with data from the created EKS cluster
+
+# Configure Kubernetes Provider
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.main.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
@@ -34,23 +38,28 @@ provider "kubernetes" {
 
 # Apply Appointment Service Deployment and Service
 resource "kubernetes_manifest" "appointment_deployment" {
-  manifest = yamldecode(file("${path.module}/kubernetes/appointment/deployment.yaml"))
+  depends_on = [aws_eks_cluster.main]
+  manifest   = yamldecode(file("${path.module}/kubernetes/appointment/deployment.yaml"))
 }
- 
+
 resource "kubernetes_manifest" "appointment_service" {
-  manifest = yamldecode(file("${path.module}/kubernetes/appointment/service.yaml"))
+  depends_on = [aws_eks_cluster.main]
+  manifest   = yamldecode(file("${path.module}/kubernetes/appointment/service.yaml"))
 }
- 
+
 # Apply Patient Service Deployment and Service
 resource "kubernetes_manifest" "patient_deployment" {
-  manifest = yamldecode(file("${path.module}/kubernetes/patient/deployment.yaml"))
+  depends_on = [aws_eks_cluster.main]
+  manifest   = yamldecode(file("${path.module}/kubernetes/patient/deployment.yaml"))
 }
- 
+
 resource "kubernetes_manifest" "patient_service" {
-  manifest = yamldecode(file("${path.module}/kubernetes/patient/service.yaml"))
+  depends_on = [aws_eks_cluster.main]
+  manifest   = yamldecode(file("${path.module}/kubernetes/patient/service.yaml"))
 }
- 
+
 # Apply Ingress for Patient Service
 resource "kubernetes_manifest" "patient_ingress" {
-  manifest = yamldecode(file("${path.module}/kubernetes/ingress.yaml"))
+  depends_on = [aws_eks_cluster.main]
+  manifest   = yamldecode(file("${path.module}/kubernetes/ingress.yaml"))
 }
