8b08b21520425387134a5c82afe50b2f3922260c
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-04-22 13:58:24 +0530

    Update outputs.tf

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -11,5 +11,5 @@ output "cluster_endpoint" {
 
 output "cluster_security_group_id" {
   description = "The security group ID of the EKS cluster"
-  value       = aws_eks_cluster.main.cluster_security_group_id
+  value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
 }
